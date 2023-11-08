ac1ad75b67a84205d1d8ffbe6df8838ee62e7442
Rancho-zhao <54349793+Rancho-zhao@users.noreply.github.com>
2023-11-08 17:06:43 +0800

    Modify fots_test.py

--- fots_test.py
+++ fots_test.py
@@ -10,30 +10,34 @@ import params as pr
 
 mm_to_pixel = pr.mm_to_pixel
 psp_w, psp_h = pr.sensor_w, pr.sensor_h
+s_type = pr.sensor_type
 
 # Translation and Rotation (mm & rad)
-trans = [0, 0, 0.2]
-rot = [0.0, 0, 0.]
+trans = [0, 0, 0.1]
+rot = [0, 0, 0]
 
 if __name__=="__main__":
     # Read the stl file as a mesh
-    mesh = o3d.io.read_triangle_mesh("assets/daniel/cone.stl")
+    mesh = o3d.io.read_triangle_mesh("assets/daniel/cylinder.stl")
+    gel_pad = np.load("utils/utils_data/"+s_type+"_pad.npy")
     model_center = mesh.get_center()
 
     R = mesh.get_rotation_matrix_from_xyz(rot) 
-    mesh.rotate(R, center=model_center) 
+    mesh.rotate(R, center=[0,0,0]) 
     mesh.compute_vertex_normals 
 
     # Sample points from the mesh
-    pcd = mesh.sample_points_uniformly(number_of_points=5000000)
+    # pcd = mesh.sample_points_poisson_disk(number_of_points=1000000, init_factor=1)
+    pcd = mesh.sample_points_uniformly(number_of_points=1000000)
+    # o3d.visualization.draw_geometries([pcd]) 
     vertices = np.asarray(pcd.points)
 
     # gel_map = np.ones((320,240)) * 0.0
     heightMap = np.zeros((psp_h,psp_w))
 
     # centralize the points
-    cx = model_center[0]#np.mean(vertices[:,0])
-    cy = model_center[1]#np.mean(vertices[:,1])
+    cx = 0.0#model_center[0]#np.mean(vertices[:,0])
+    cy = 0.0#model_center[1]#np.mean(vertices[:,1])
 
     # add the shifting and change to the pix coordinate
     uu = ((vertices[:,0] - cx + trans[0])*mm_to_pixel + psp_w//2).astype(int)
@@ -45,6 +49,8 @@ if __name__=="__main__":
     mask_z = vertices[:,2] > 10
     mask_map = mask_u & mask_v & mask_z
     heightMap[vv[mask_map],uu[mask_map]] = vertices[mask_map][:,2]*mm_to_pixel
+
+    heightMap += gel_pad*mm_to_pixel
 
     max_o = np.max(heightMap)
     gel_map = np.ones((psp_h, psp_w)) * max_o
@@ -73,7 +79,8 @@ if __name__=="__main__":
         relative_pos.append([0, 0, 0])
         relative_pos.append([trans[0], trans[1], rot[2]])
     # obtain markers' motion according to depth and object geometry info
-    marker = MarkerMotion(frame0_blur=tact_img,depth=zq/mm_to_pixel,mask=contact_mask,traj=relative_pos,lamb=[0.00125,0.00021,0.00038])
+    marker = MarkerMotion(frame0_blur=tact_img,depth=zq/mm_to_pixel,mask=contact_mask,traj=relative_pos,
+                          lamb=[0.00125,0.00021,0.00038])
     marker_img = marker._marker_motion()
 
     cv2.imshow("tact_img",tact_img)
