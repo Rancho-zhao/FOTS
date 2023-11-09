ca3dfb9be4d1b1d411362f3f13d63a665b6fc4ff
Rancho-zhao <54349793+Rancho-zhao@users.noreply.github.com>
2023-11-09 11:32:49 +0800

    Modify params.py

--- params.py
+++ params.py
@@ -8,5 +8,23 @@ sensor_w = 240
 
 sensor_type = 'gelsight' # gelsight
 
+# optical render params
 pyramid_kernel_size = [51, 21, 11, 5]#[11, 11, 11, 11, 11, 5] 
 kernel_size = 5
+
+# marker params
+## DIGIT
+# N = 9
+# M = 8
+# x0 = 40
+# y0 = 27
+# dx = 27
+# dy = 27
+
+## GelSight
+N = 11
+M = 9
+x0 = 26
+y0 = 15
+dx = 29
+dy = 26
